cd3409a6e757ee3b5d351a36a782d6097d2eeba6
Samer A <thedev.samer@gmail.com>
2022-06-14 12:24:12 +0300

    fixed routing issue & added the portfolio route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import styled from "styled-components";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
+import Portfolio from "./pages/Portfolio";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <Container>
       <Routes>
+        <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/not-found" element={<NotFound />} />
         <Route path="/" element={<Home />} />
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+
+const Portfolio = () => {
+  return (
+    <Container>
+      <Navbar page="portfolio" />
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+
+export default Portfolio;
