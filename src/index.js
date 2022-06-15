64e0dd100cae5cd6f3a6ad218df835d8638693ea
Samer A <thedev.samer@gmail.com>
2022-06-15 15:32:27 +0300

    removed redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import configureStore from "./store/configureStore";
 import App from "./App";
 import "./index.css";
-
-const store = configureStore();
 
 ReactDOM.render(
   <BrowserRouter>
